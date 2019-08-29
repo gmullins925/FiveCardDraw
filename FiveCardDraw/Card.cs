82b611543e22fa7edc2fd82a9b5b2970f9e6b249
gmullins925 <50452654+gmullins925@users.noreply.github.com>
2019-08-29 14:11:06 -0500

    Class EOD

---
+++ FiveCardDraw/Card.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Example_VS
+{
+	class Card
+	{
+
+		//card data
+		string rank;
+		string suit;
+		int val;
+
+
+		/// <summary>
+		/// Gets or sets an integer valuefor the card. Different card games
+		/// would assign different values.
+		/// </summary>
+
+
+		/// <summary>
+		/// Creates a Card object with the given card rank and suit
+		/// </summary>
+		/// <param name="rank">A, K, Q, J, 10...2</param>
+		/// <param name="suit">Use ASCII char for club, spade, heart or diamond</param>
+		public Card(string rank, string suit, int val)
+		{
+			this.rank = rank;
+			this.suit = suit;
+			this.val = val;
+		}
+
+		public int Value
+		{
+			get
+			{
+				return val;
+			}
+			set
+			{
+				val = Value;
+			}
+		}
+
+		public string Rank { get => rank; set => rank = value; }
+		public string Suit { get => suit; set => suit = value; }
+		public int Val { get => val; set => val = value; }
+
+		public override string ToString()
+		{
+			return rank + " " + suit;
+		}
+
+
+	}
+}
